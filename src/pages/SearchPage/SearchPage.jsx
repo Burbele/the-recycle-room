3aa5557d5f00ac928c512d05f147b9cd0106f2f8
Burbele <eaapbur@students.eaaa.dk>
2023-12-11 00:01:02 +0100

    Styling

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,13 +1,23 @@
 import "../../pages/SearchPage/SearchPage.css";
 import Header from "../../components/Header/Header";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import RecyclingImage from "../../assets/recycling-image.png";
+import "./SearchPage.css";
 
 function SearchPage() {
   return (
     <>
       <div className="page">
         <Header />
-        <h1>How do I recycle this?</h1>
+        <div className="container-cover">
+          <img
+            className="image-style-cover"
+            src={RecyclingImage}
+          />
+          <div className="text-block">
+            <h1>How do I recycle this?</h1>
+          </div>
+        </div>
         <SearchBar />
       </div>
     </>
