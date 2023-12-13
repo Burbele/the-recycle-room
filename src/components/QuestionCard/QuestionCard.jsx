4d957791fdb43c671ef42e932b9e97f07d58a097
Burbele <eaapbur@students.eaaa.dk>
2023-12-13 12:36:26 +0100

    search bar fix

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,3 +1,4 @@
+// QuestionCard.jsx
 import { useState, useEffect } from "react";
 import { ref, onValue } from "firebase/database";
 import { db } from "./../../firebase-config";
@@ -8,11 +9,13 @@ import "./QuestionCard.css";
 
 function QuestionCard({ searchValue }) {
   const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const questionsRef = ref(db, "questions");
 
     onValue(questionsRef, (snapshot) => {
+      setLoading(false);
       if (snapshot.exists()) {
         const questionsArray = Object.entries(snapshot.val()).map(([key, value]) => ({
           id: key,
@@ -25,31 +28,37 @@ function QuestionCard({ searchValue }) {
     });
   }, []);
 
-  // Filter questions based on searchValue
-  const filteredQuestions = questions.filter((question) =>
-    question.question.toLowerCase().includes(searchValue.toLowerCase())
-  );
-
-  if (questions.length === 0) {
+  if (loading) {
     return <p>Loading...</p>;
   }
 
+  // If searchValue is empty, display all questions
+  const displayedQuestions = searchValue
+    ? questions.filter((question) =>
+        question.question.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    : questions;
+
   return (
     <div>
-      {filteredQuestions.map((question) => (
-        <div
-          key={question.id}
-          className="question-container">
-          <article className="display-container">
-            <div className="title-container">
-              <p className="question-author">{question.username} asked a question</p>
-              <p className="question-title">{question.question}</p>
-              <p className="question-answer">{question.answerDisplay}</p>
-            </div>
-            <FaArrowRight className="arrow-right" />
-          </article>
-        </div>
-      ))}
+      {displayedQuestions.length === 0 ? (
+        <p className="not-found">No matching questions found.</p>
+      ) : (
+        displayedQuestions.map((question) => (
+          <div
+            key={question.id}
+            className="question-container">
+            <article className="display-container">
+              <div className="title-container">
+                <p className="question-author">{question.username} asked a question</p>
+                <p className="question-title">{question.question}</p>
+                <p className="question-answer">{question.answerDisplay}</p>
+              </div>
+              <FaArrowRight className="arrow-right" />
+            </article>
+          </div>
+        ))
+      )}
     </div>
   );
 }
